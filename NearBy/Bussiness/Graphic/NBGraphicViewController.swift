babfdf416693755a5e636699f96d4b5636ab439c
zx <zx19880528@gmail.com>
2016-11-04 17:49:34 +0800

    zx

--- NearBy/Bussiness/Graphic/NBGraphicViewController.swift
+++ NearBy/Bussiness/Graphic/NBGraphicViewController.swift
@@ -9,37 +9,29 @@
 import UIKit
 
 class NBGraphicViewController: UIViewController {
+    var topoView: TopographyView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "蓝牙拓扑图"
         view.backgroundColor = UIColor.white
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        topoView = TopographyView()
+        view.addSubview(topoView)
+        
+        let rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(NBGraphicViewController.getServerData))
+        navigationItem.rightBarButtonItem = rightBarButtonItem
     }
     
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        topoView.frame = view.bounds
     }
-    */
 
     func getServerData() {
-        //http://10.5.1.249:3000/data/getData
         let message = AIMessage()
-        message.url = "http://171.221.254.231:3003/data/getData"
-
+        message.url = "http://171.221.254.231:3003/data/getAllPoints"
         AINetEngine.default().post(message, success: { (response) -> Void in
-
+            print(response)
         }, fail: { (ErrorType, error) -> Void in
 
         })
---
+++ NearBy/Bussiness/Graphic/TopographyView.swift
@@ -0,0 +1,65 @@
+//
+//  TopographyView.swift
+//  topographyDemo
+//
+//  Created by zx on 10/28/16.
+//  Copyright © 2016 zx. All rights reserved.
+//
+
+import UIKit
+
+struct TopoPoint {
+    var x: CGFloat
+    var y: CGFloat
+    var id: String
+}
+
+class TopographyView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
+        layer.masksToBounds = false
+        clipsToBounds = false
+        updatePoints()
+    }
+    func updatePoints() {
+        pointViews.forEach { (l) in
+            l.removeFromSuperview()
+        }
+        pointViews.removeAll()
+        let radius: CGFloat = 8
+        let mutiplier: CGFloat = CGFloat(30)
+        for (i,p) in points.enumerated() {
+            let xCoord = p.x * mutiplier
+            let yCoord = p.y * mutiplier
+            let label = UILabel()
+            label.text = p.id
+            label.clipsToBounds = true
+            label.layer.cornerRadius = 4
+            label.textAlignment = .center
+            label.backgroundColor = UIColor.red
+            label.layer.borderColor = UIColor.black.cgColor
+            label.layer.borderWidth = 2
+            label.frame = CGRect(x: xCoord, y: yCoord, width: radius*3, height: radius*3)
+            addSubview(label)
+            pointViews.append(label)
+        }
+    }
+    
+    
+    
+    var pointViews: [UILabel] = []
+    var points: [TopoPoint] = [] {
+        didSet {
+            updatePoints()
+        }
+    }
+}
